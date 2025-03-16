4b043d43f8c3bbc92ed51bbff18407376146dc9e
spacefall <lucarichu@outlook.it>
2025-03-16 18:59:36 +0100

    feat: better help for translate

--- cli/config.go
+++ cli/config.go
@@ -8,9 +8,9 @@ import (
 
 var HelpStr = fmt.Sprintf(`Usage: %s [word] [options]
 Options:
-  -th, --thesaurus    Print synonyms and antonyms
-  -tr, --translate    Translate the word to another language
-  -h, --help          Print this help message`, os.Args[0])
+  -th, --thesaurus   	Print synonyms and antonyms
+  -tr, --translate <LC>	Translate the word to another language, LC is the 2 letter language code (like en, it)
+  -h, --help          	Print this help message`, os.Args[0])
 
 type Params struct {
 	Word        string
